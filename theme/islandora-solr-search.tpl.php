5829bee9c0192dc24f7b2ebffbd286ea294f5cf7
Danny Joris <danny_joris@yahoo.co.uk>
2012-05-09 11:11:33 -0300

    renamed the two search results template files

--- theme/islandora-solr-search.tpl.php
+++ theme/islandora-solr-search.tpl.php
@@ -2,28 +2,43 @@
 
 /**
 * @file islandora-solr-search.tpl.php
-* Islandora solr search results template
+* Islandora solr search primary results template file
 *
 * Variables available:
 * - $variables: all array elements of $variables can be used as a variable. e.g. $base_url equals $variables['base_url']
 * - $base_url: The base url of the current website. eg: http://example.com .
 * - $user: The user object.
 *
-* - $secondary_profiles: Rendered secondary profiles
-* - $results: Rendered search results (primary profile)
-* - $islandora_solr_result_count: Solr result count string
-* - $solrpager: The pager
-* - $solr_debug: debug info
-*
+* - $results: Primary profile results array
 */
+
 ?>
 
-<?php print $secondary_profiles; ?>
-<h2 class="title"><?php print $solr_title; ?></h2>
-<div id="islandora-solr-result-count"><?php print $islandora_solr_result_count; ?></div>
-<div class="islandora-solr-content">
-  <?php print $solr_pager; ?>
-  <?php print $results; ?>
-  <?php print $solr_debug; ?>
-  <?php print $solr_pager; ?>
-</div>
+<?php if (empty($results)): ?>
+  <p><?php print t('Sorry, but your search returned no results.'); ?></p>
+<?php else: ?>
+  <!-- Ordered list -->
+  <ol class="islandora-solr-search-results" start="<?php print $solr_start + 1 ?>">
+    <?php $row_result = 0; ?>
+    <?php foreach($results as $result): ?>
+    <!-- add first/last classes + zebra -->
+    <li class="<?php print $row_result % 2 == 0 ? 'odd' : 'even'; ?>">
+      <!-- Table -->
+      <table class="islandora-solr-search-result">
+        <?php $row_field = 0; ?>
+        <?php foreach($result as $key => $value): ?>
+          <tr class="<?php print $value['class']; ?> <?php print $row_field % 2 == 0 ? 'odd' : 'even'; ?>">
+            <th class="solr-label"><?php print $value['label']; ?></th>
+            <?php if ($key == 'PID'): ?>
+              <?php $value['value'] = l($value['value'], 'fedora/repository/' . htmlspecialchars($value['value'], ENT_QUOTES, 'utf-8')); ?>
+            <?php endif; ?>
+            <td class="solr-value"><?php print $value['value']; ?></td>
+          </tr>
+          <?php $row_field++; ?>
+        <?php endforeach; ?>
+      </table>
+    </li>
+    <?php $row_result++; ?>
+    <?php endforeach; ?>
+  </ol>
+<?php endif; ?>
---
+++ theme/islandora-solr-wrapper.tpl.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+* @file islandora-solr-wrapper.tpl.php
+* Islandora solr search results wrapper template
+*
+* Variables available:
+* - $variables: all array elements of $variables can be used as a variable. e.g. $base_url equals $variables['base_url']
+* - $base_url: The base url of the current website. eg: http://example.com .
+* - $user: The user object.
+*
+* - $secondary_profiles: Rendered secondary profiles
+* - $results: Rendered search results (primary profile)
+* - $islandora_solr_result_count: Solr result count string
+* - $solrpager: The pager
+* - $solr_debug: debug info
+*
+*/
+?>
+
+<?php print $secondary_profiles; ?>
+<h2 class="title"><?php print $solr_title; ?></h2>
+<div id="islandora-solr-result-count"><?php print $islandora_solr_result_count; ?></div>
+<div class="islandora-solr-content">
+  <?php print $solr_pager; ?>
+  <?php print $results; ?>
+  <?php print $solr_debug; ?>
+  <?php print $solr_pager; ?>
+</div>
