c59c44e669751db472e54f0813173dd318eb87d0
Danny Joris <danny_joris@yahoo.co.uk>
2012-05-22 13:43:42 -0300

    moved some classes around in the template file - some cleanup

--- theme/islandora-solr-search.tpl.php
+++ theme/islandora-solr-search.tpl.php
@@ -11,42 +11,43 @@
 *
 * - $results: Primary profile results array
 */
-
 ?>
 
 <?php if (empty($results)): ?>
   <p class="no-results"><?php print t('Sorry, but your search returned no results.'); ?></p>
 <?php else: ?>
-  <div class="islandora-solr-search-results" start="<?php print $solr_start + 1 ?>">
+  <div class="islandora-solr-search-results">
     <?php $row_result = 0; ?>
     <?php foreach($results as $result): ?>
-
-      <div class="clear-block">
+    <!-- Search result -->
+    <div class="islandora-solr-search-result clear-block <?php print $row_result % 2 == 0 ? 'odd' : 'even'; ?>">
       <!-- Thumbnail -->
-      <dl class="islandora-solr-search-thumb">
+      <dl class="solr-thumb">
         <dt>
-          <?php $image = '<img src="' . $base_url . '/fedora/repository/' . $result['PID']['value'] . '/TN"' . 'onerror="this.src=\'' . $solr_default_img . '\'"' . '/>'; ?>
+          <?php $image = '<img src="' . $base_url . '/fedora/repository/' . $result['PID']['value'] . '/TN"' . ' onerror="this.src=\'' . $solr_default_img . '\'" />'; ?>
           <?php print l($image, 'fedora/repository/' . $result['PID']['value'], array('html' => TRUE, 'attributes' => array('title' => $result['mods_title_mlt']['value']))); ?>
         </dt>
         <dd></dd>
       </dl>
-      
       <!-- Metadata -->
-      <dl class="islandora-solr-search-result">
+      <dl class="solr-fields">
         <?php $row_field = 0; ?>
         <?php $max_rows = count($results[$row_result]) - 1; ?>
         <?php foreach($result as $key => $value): ?>
-          <dt class="solr-label <?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?><?php print $row_field == $max_rows ? ' last' : ''; ?>"><?php print $value['label']; ?></dt>
-            <?php if ($key == 'PID'): ?>
-              <?php $value['value'] = l($value['value'], 'fedora/repository/' . htmlspecialchars($value['value'], ENT_QUOTES, 'utf-8')); ?>
-            <?php endif; ?>
-          <dd class="solr-value <?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?><?php print $row_field == $max_rows ? ' last' : ''; ?>"><?php print $value['value']; ?></dd>
+          <dt class="solr-label <?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?><?php print $row_field == $max_rows ? ' last' : ''; ?>">
+            <?php print $value['label']; ?>
+          </dt>
+          <?php if ($key == 'PID'): ?>
+            <?php $value['value'] = l($value['value'], 'fedora/repository/' . htmlspecialchars($value['value'], ENT_QUOTES, 'utf-8')); ?>
+          <?php endif; ?>
+          <dd class="solr-value <?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?><?php print $row_field == $max_rows ? ' last' : ''; ?>">
+            <?php print $value['value']; ?>
+          </dd>
           <?php $row_field++; ?>
         <?php endforeach; ?>
       </dl>
-      </div>
-
+    </div>
     <?php $row_result++; ?>
     <?php endforeach; ?>
-
+  </div>
 <?php endif; ?>
