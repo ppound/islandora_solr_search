fdc0e8e48dac23f36fc660371e7f335b2f7b67c2
Danny Joris <danny_joris@yahoo.co.uk>
2012-05-24 15:10:58 -0300

    updated soft limit js function

--- js/islandora_solr_soft_limit.js
+++ js/islandora_solr_soft_limit.js
@@ -1,12 +1,20 @@
-$(document).ready(function() {
+(function($) {
   
-  // show more  
-  $(".soft-limit").click(function() {
+  // snippet for facet block toggle
+  Drupal.behaviors.islandora_solr_toggle = {
+    attach: function(context, settings) {
+      // show more  
+      if (!$(".soft-limit").hasClass('processed')) {
+        $(".soft-limit").click(function(e) {
+          // toggle class .hidden
+          $(this).siblings(".hidden, .toggle-wrapper").toggleClass('hidden');
+          $(this).toggleClass('hidden');
+
+          e.preventDefault();
+        });
+        $(".soft-limit").addClass('processed');
+      }
+    }
+  };
   
-    $(this).siblings(".hidden, .toggle-wrapper").toggleClass('hidden');
-    $(this).toggleClass('hidden');
-
-    return false;
-   });
-
-});
+}(jQuery));   
