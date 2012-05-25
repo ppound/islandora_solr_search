11589931e10d954170b62e59b1e7f7ae1aa6d201
Danny Joris <danny_joris@yahoo.co.uk>
2012-05-25 17:11:34 -0300

    first phase of moving extended advanced search form into islanodra_solr

--- extended_search_form/js/islandora_extended_search_form-ahah.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $("body").ajaxComplete(function(event, request, settings) {
-    var response = eval("(" + request.responseText + ")");
-    jQuery.extend(Drupal.settings, response.settings);
-    Drupal.attachBehaviors();
-  });
-});
