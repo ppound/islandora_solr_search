30d369831a9e0e6f55946c6c4b84c2cba9b0490a
Danny Joris <danny_joris@yahoo.co.uk>
2012-05-31 18:11:29 -0300

    added islandora_solr.api.php to document hooks provided by islandora_solr. + made some doxygen tweaks here and there.

---
+++ islandora_solr.api.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by Islandora Solr.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Hook to collect data to set up a primary display profile.
+ *
+ * @return
+ *   Returns an array with all the data required to build a primary display
+ *   profile.
+ */
+function hook_islandora_solr_primary_display() {
+  return array(
+    'machine_name' => array(
+      'name' => t('Human readable name'),
+      'module' => 'module_name',
+      'file' => 'FileName.inc',
+      'class' => 'ClassName',
+      'function' => 'function_name',
+      'description' => t('A description of the display profile'),
+      'configuration' => 'path/to/configuration/page',
+    ),
+  );
+}
+
+/**
+ * Hook to collect data to set up a secondary display profile.
+ *
+ * @return
+ *   Returns an array with all the data required to build a secondary display
+ *   profile.
+ */
+function hook_islandora_solr_secondary_display() {
+  return array(
+    'machine_name' => array(
+      'name' => t('Human readable name'),
+      'module' => 'module_name',
+      'file' => 'FileName.inc',
+      'class' => 'ClassName',
+      'function' => 'function_name',
+      'description' => t('A description of the display profile'),
+      'logo' => '<img src="path/to/icon.png">',
+    ),
+  );
+}
+
+/**
+ * Hook which passes the IslandoraSolrQueryProcessor object right before it
+ * connects to Solr and executes it. This is a hook to call when you want to
+ * make 'last minute' changes.
+ *
+ * @param type $queryProcessor
+ *   The IslandoraSolrQueryProcessor object which includes the current query
+ *   settings but before it calls IslandoraSolrQueryProcessor::executeQuery().
+ */
+function hook_islandora_solr_query_processor($queryProcessor) {
+  // example: on example_display, always sort descending on fgs.createdDate
+  if ($queryProcessor->display == 'example_display') {
+    $queryProcessor->solrParams['sort'] = 'fgs.createdDate desc';
+  }
+}
+
+/**
+ * Hook to collect data to populate block Islandora Solr blocks.
+ *
+ * Note: it is valid to specify *either* a class and method *or* a form. The
+ * latter implies no class needs to be instantiated.
+ *
+ * @return
+ *   Returns an array with all the data required to build blocks. These include
+ *   references to specific modules/methods/classes/functions to call in
+ *   hook_block_view().
+ */
+function hook_islandora_solr_query_blocks() {
+  return array(
+    'machine_name' => array(
+      'name' => t('Human Readable Name'),
+      'module' => 'module_name',
+      'file' => 'FileName.inc',
+      'class' => 'ClassName',
+      'function' => 'method_name',
+      'form' => 'form_function_name',
+    ),
+  );
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
