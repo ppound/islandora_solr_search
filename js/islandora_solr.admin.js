0b619987b11f9efb22df46ab31b15420bb43a4fc
Danny Joris <danny_joris@yahoo.co.uk>
2012-06-29 17:21:21 -0300

    added a field table for sort fields as well. Adding a second table came with quite some issues, which are fixed in this commit.

--- js/islandora_solr.admin.js
+++ js/islandora_solr.admin.js
@@ -1,16 +1,22 @@
-
+/* Script for islandora_solr admin */
 (function ($) {
-
+console.log('js');
+  // function to trigger a form buttom when clicking on a link element.
   Drupal.behaviors.islandora_solr_remove = {
     attach: function(context, settings) {
       // show more
-      if (!$(".islandora-solr-remove-link").hasClass('processed')) {
-        $('.islandora-solr-remove-link').click(function() {
-          $(this).parent('.islandora-solr-operations').next('td').find('input').trigger('click');
-          return false;
-        });
-        $(".islandora-solr-remove-link").addClass('processed');
-      }
+      $(".islandora-solr-remove-link").each(function () {
+
+        if (!$(this).hasClass('processed')) {
+          $('.islandora-solr-remove-link').click(function() {
+            $(this).parent('.islandora-solr-operations').next('td').find('input').trigger('click');
+            return false;
+          });
+          $(".islandora-solr-remove-link").addClass('processed');
+        }
+
+      });
+      
     }
   };
 
