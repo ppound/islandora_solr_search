91793c41026af6a6dfe2fb9ae9c3d9e1392f6eb0
Danny Joris <danny_joris@yahoo.co.uk>
2012-07-18 16:47:13 -0300

    first phase of adding slider and preparing data for it

--- js/islandora_solr_search_facets.js
+++ js/islandora_solr_search_facets.js
@@ -66,3 +66,52 @@ Drupal.behaviors.dateRangeFilterDatepicker = function (context) {
   });
 }
 
+
+
+// Range slider
+Drupal.behaviors.dateRangeSlider = function (context) {
+
+  // get year range variable
+  var rangeSliderVals = Drupal.settings.islandora_solr_search_range_slider;
+
+  
+  $.each(rangeSliderVals, function() {
+    var sliderData = this.data;
+    var form_key = this.form_key;
+    var sliderId = '#date-range-slider-' + form_key;
+    var amountId = '#slider-amount-' + form_key;
+    
+    var sliderValue = 0;
+    var sliderMax = sliderData.length - 1;
+    var sliderMin = 0;
+    var sliderStep = 1;
+
+
+    $(sliderId).slider({
+      range: true,
+      handles: [{start:sliderMin, min:sliderMin, max:sliderMax, id:'range-slider-handle-min-' + form_key}, {start:sliderMax, min:sliderMin, max:sliderMax, id:'range-slider-handle-max-' + form_key}],
+      values: [sliderMin, sliderMax],
+      min: sliderMin,
+      max: sliderMax,
+      stepping: sliderStep, // this is named 'step' in newer versions of jquery ui
+      change: function(event, ui) {
+        // assign values to form fields. // @TODO: this is done differently in newer versions of jquery ui        
+        // min handler: assign to 'from' field
+        if (ui.handle[0].id == 'range-slider-handle-min-' + form_key) {
+          var fromDate = sliderData[ui.value].date;
+          $('.range-slider-hidden-from-' + form_key).val(fromDate);
+        }
+        // max handler: assign to 'to' field
+        if (ui.handle[0].id == 'range-slider-handle-max-' + form_key) {
+          var toDate = sliderData[ui.value].date;
+          $('.range-slider-hidden-to-' + form_key).val(toDate);
+        }
+console.log(ui.value);
+//        $(amountId).html(ui.value);
+      }
+    });
+
+    $(amountId).html($(sliderId).slider("value"));
+  });
+}
+
